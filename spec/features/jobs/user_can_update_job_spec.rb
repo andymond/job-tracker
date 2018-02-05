c289c3399adcc387956d43964d36d49ed53d3234
Andrew Dymond <andy.dymond@gmail.com>
2018-02-05 15:35:54 -0700

    Update job complete with spec passing

--- spec/features/jobs/user_can_update_job_spec.rb
+++ spec/features/jobs/user_can_update_job_spec.rb
@@ -6,10 +6,16 @@ describe "User navigates to edit job" do
     job = company.jobs.create(title: "Cool job", level_of_interest: 80, description: "Wahoo", city: "Denver")
 
     visit edit_company_job_path(company, job)
+    fill_in "job[title]", with: "Even cooler job"
+    fill_in "job[level_of_interest]", with: 100
+    fill_in "job[description]", with: "Do cool sports reporting stuff"
+    fill_in "job[city]", with: "Brooklyn"
+    click_on "Update Job"
 
     expect(page).to have_content("ESPN")
-    expect(page).to have_content("80")
-    expect(page).to have_content("Wahoo")
-    expect(page).to have_content("Denver")
+    expect(page).to have_content("Even cooler job")
+    expect(page).to have_content(100)
+    expect(page).to have_content("Do cool sports reporting stuff")
+    expect(page).to have_content("Brooklyn")
   end
 end

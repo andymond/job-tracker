1c33bf8d4c88c66b3524a9778b11f711b0c5eb91
Andrew Dymond <andy.dymond@gmail.com>
2018-02-06 16:06:26 -0700

    Add spec for contact model

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe Contact type: :model do
+  describe "validations" do
+    context "valid attributes" do
+      it "is valid with name, title, email and company" do
+        company = Company.new(name: "CorpCorp")
+        contact = company.contacts.new(name: "Mr. T", title: "HR person", email: "email@example.com")
+
+        expect(contact).to be_valid
+      end
+      it "is valid with title, email and company" do
+        company = Company.new(name: "CorpCorp")
+        contact = company.contacts.new(title: "HR person", email: "email@example.com")
+
+        expect(contact).to be_valid
+      end
+    end
+    context "invalid attributes" do
+      it "is not valid without title" do
+        company = Company.new(name: "CorpCorp")
+        contact = company.contacts.new(name: "Mr. T", email: "email@example.com")
+
+        expect(contact).to be_invalid
+      end
+      it "is not valid without email" do
+        company = Company.new(name: "CorpCorp")
+        contact = company.contacts.new(name: "Mr. T", title: "HR person")
+
+        expect(contact).to be_invalid
+      end
+      it "is not valid without company" do
+        contact = Contact.new(name: "humfry", title: "HR person", email: "email@example.com")
+
+        expect(contact).to be_invalid
+      end
+    end
+  end
+  describe "relationships" do
+    it {should belong_to(:company)}
+  end
+end

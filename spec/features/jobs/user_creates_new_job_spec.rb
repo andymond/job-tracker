4b2d21a8edc55192c15f5d0fcb0f77f6a90d06c3
Andrew Dymond <andy.dymond@gmail.com>
2018-02-06 13:13:59 -0700

    Add spec & implement link to new category from new job

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "User creates a new job" do
-  scenario "a user can create a new job" do
+  it "allows user to create a new job" do
     company = Company.create!(name: "ESPN")
     category = Category.create(title: "Sportsy Job")
     visit new_company_job_path(company)
@@ -19,5 +19,13 @@ describe "User creates a new job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("80")
     expect(page).to have_content("Denver")
+  end
+  it "allows user to navigate to new category" do
+    company = Company.create!(name: "ESPN")
+
+    visit new_company_job_path(company)
+    click_on "Create A New Category"
+
+    expect(current_path).to eq(new_category_path)
   end
 end

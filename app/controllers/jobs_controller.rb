85db8d18d76a9e8004aee0db29f13a6231e0ffca
Andrew Dymond <andy.dymond@gmail.com>
2018-02-05 17:32:50 -0700

    Set instance variables in jobscontroller in before_action to refactor

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,16 @@
 class JobsController < ApplicationController
+  before_action :set_company, except: [:show]
+  before_action :set_job, except: [:index, :new, :create]
+
   def index
-    @company = Company.find(params[:company_id])
     @jobs = @company.jobs
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new()
   end
 
   def create
-    @company = Company.find(params[:company_id])
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}."
@@ -21,17 +21,14 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.find(params[:id])
+
   end
 
   def edit
-    @company = Company.find(params[:company_id])
-    @job = Job.find(params[:id])
+
   end
 
   def update
-    @company = Company.find(params[:company_id])
-    @job = Job.find(params[:id])
     if @job.update(job_params)
       flash[:success] = "You updated #{@job.title} at #{@company.name}."
       redirect_to company_job_path(@company, @job)
@@ -43,7 +40,6 @@ class JobsController < ApplicationController
   end
 
   def destroy
-    @job = Job.find(params[:id])
     @job.destroy
 
     flash.notice = "#{@job.title} was deleted."
@@ -55,5 +51,13 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :description, :level_of_interest, :city)
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_company
+    @company = Company.find(params[:company_id])
   end
 end
